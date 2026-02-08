e54125156ffeafbd37977badb774c8e294581550
Miguel Royo <miguelroyo018@gmail.com>
2026-02-08 13:25:00 +0100

    task: create and style index page
    Closes #4

--- magnate/src/App.tsx
+++ magnate/src/App.tsx
@@ -1,21 +1,18 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
-import Login from '@/pages/Login/Login'
-import SignUp from '@/pages/SignUp/SignUp'
-import Loading from '@/pages/Loading/Loading'
-import { PageHeader } from '@/components/layout/PageHeader'
-import { GameMode } from '@/pages/GameMode/GameMode'
+import { Routes, Route } from "react-router-dom";
+import { LandingPage } from "@/pages/LandingPage";
+import { Login } from "@/pages/Login/Login";
+import { SignUp } from "@/pages/SignUp/SignUp";
+import { GameMode } from "@/pages/GameMode/GameMode";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <GameMode />
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/game-mode" element={<GameMode />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
